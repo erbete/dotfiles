f33919f3384f3b12bc37511304cbee5394a7c2f8
erbete <68850523+erbete@users.noreply.github.com>
2022-09-18 21:41:28 +0200

    add indent settings + other stuff

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -8,21 +8,24 @@ vim.wo.number = true
 vim.opt.fixendofline = false
 vim.opt.showmode = false
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.o.showcmd = false 
+vim.o.showcmd = false
 vim.opt.cmdheight = 1
 vim.opt.pumheight = 10
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 vim.opt.swapfile = false
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamed,unnamedplus"
 vim.opt.completeopt = "menuone,noselect"
 vim.opt.signcolumn = "yes"
 vim.opt.ruler = false
 vim.opt.virtualedit = "block"
+vim.opt.smarttab = true
+vim.opt.breakindent = true
 
 -- 4 spaces
 vim.opt.expandtab = true

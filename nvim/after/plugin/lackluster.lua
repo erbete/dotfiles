f9814dfed74b37804552bbd6942fcb8bca481c33
erbete <68850523+erbete@users.noreply.github.com>
2025-08-04 21:28:25 +0200

    add vesper theme

--- nvim/after/plugin/lackluster.lua
+++ nvim/after/plugin/lackluster.lua
@@ -1,7 +1,7 @@
-local status, github = pcall(require, "slugbyte/lackluster.nvim")
+local status, lackluster = pcall(require, "slugbyte/lackluster.nvim")
 if (not status) then return end
 
-github.setup({
+lackluster.setup({
     lazy = false,
     priority = 1000,
     init = function()
